968a31e308e7e005aeb01374b793a08463e88187
MDN37 <45273378+MDN37@users.noreply.github.com>
2026-03-26 13:57:50 +0300

    iqos_truing

---
+++ iqos_try.py
@@ -0,0 +1,58 @@
+from playwright.sync_api import sync_playwright, expect
+import local_settings as settings
+
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    page.goto("https://iqos-alb.test.cleverbots.ru/admin/login/?next=/admin/")
+
+    login = page.locator('//input[@id="id_username"]')
+    login.fill(settings.USER)
+
+    password_input = page.locator('//input[@id="id_password"]')
+    password_input.fill(settings.PASSWORD)
+
+    login_button = page.locator('//input[@value="Log in"]')
+    login_button.click()
+
+    user_button = page.locator('//div[@class="app-auth module"]//a[@href="/admin/auth/user/" and text()="Users"]')
+    user_button.click()
+
+    add_button = page.locator('//div[@id="content-main"]//a[@class="addlink"]')
+    add_button.click()
+
+    login2 = page.locator('//input[@id="id_username"]')
+    login2.fill('Auto_test')
+
+    password_input2 = page.locator('//input[@id="id_password1"]')
+    password_input2.fill('sR58Dc8z')
+
+    password_input3 = page.locator('//input[@id="id_password2"]')
+    password_input3.fill('sR58Dc8z')
+
+    save_button = page.locator('//input[@class="default" and @value="Save"]')
+    save_button.click()
+
+    page.goto("https://iqos-alb.test.cleverbots.ru/admin/auth/user/")
+
+    page.wait_for_timeout(5000)
+
+    user_button = page.locator('//a[text()="Auto_test"]')
+    user_button.click()
+
+    delete_button = page.locator('//a[@class="deletelink"]')
+    delete_button.click()
+
+    sure_button = page.locator('//input[@value="Yes, I’m sure"]')
+    sure_button.click()
+
+    wrong_alert = page.locator('//li[@class="success"]')
+    expect(wrong_alert).to_be_visible()
+    expect(wrong_alert).to_have_text(
+        'The user “Auto_test” was deleted successfully.'
+    )
+
+
+    page.wait_for_timeout(3000)
