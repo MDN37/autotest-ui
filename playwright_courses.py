8ea082ffb86ed1b97526023e26dc5d3cc7a2877a
MDN37 <45273378+MDN37@users.noreply.github.com>
2026-03-27 16:22:52 +0300

    Работа с авторизацией в Playwright. Практика работы с авторизацией в Playwright

--- playwright_courses.py
+++ playwright_courses.py
@@ -5,18 +5,27 @@ with sync_playwright() as playwright:
     context = browser.new_context()
     page = context.new_page()
 
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    page.goto(
+        (
+            "https://nikita-filonov.github.io/"
+            "qa-automation-engineer-ui-course/#/auth/registration"
+        )
+    )
 
-    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input = page.get_by_test_id(
+        'registration-form-email-input').locator('input')
     email_input.fill('user@gmail.com')
 
-    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input = page.get_by_test_id(
+        'registration-form-username-input').locator('input')
     username_input.fill('username')
 
-    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input = page.get_by_test_id(
+        'registration-form-password-input').locator('input')
     password_input.fill('password')
 
-    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button = page.get_by_test_id(
+        'registration-page-registration-button')
     registration_button.click()
 
     context.storage_state(path='browser-state.json')
@@ -26,7 +35,12 @@ with sync_playwright() as playwright:
     context = browser.new_context(storage_state='browser-state.json')
     page = context.new_page()
 
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+    page.goto(
+        (
+            "https://nikita-filonov.github.io/"
+            "qa-automation-engineer-ui-course/#/courses"
+        )
+    )
 
     courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
     expect(courses_title).to_have_text(
@@ -41,7 +55,8 @@ with sync_playwright() as playwright:
     empty_block = page.get_by_test_id('courses-list-empty-view-icon')
     expect(empty_block).to_be_visible()
 
-    description_text = page.get_by_test_id('courses-list-empty-view-description-text')
+    description_text = page.get_by_test_id(
+        'courses-list-empty-view-description-text')
     expect(description_text).to_have_text(
         'Results from the load test pipeline will be displayed here'
     )
