943a4e91b3f01934427a833ea7ed079df30b320a
MDN37 <45273378+MDN37@users.noreply.github.com>
2026-04-01 15:16:40 +0300

    Параметризация Pytest. Практика работы с pytest параметризацией

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,30 +1,40 @@
 from playwright.sync_api import expect, Page
 import pytest
 
+
 @pytest.mark.regression
 @pytest.mark.authorization
-def test_wrong_email_or_password_authorization(chromium_page: Page):
+@pytest.mark.parametrize(
+    'email, password',
+    [
+        ("user.name@gmail.com", "password"),
+        ("user.name@gmail.com", "  "),
+        ("  ", "password")
+    ]
+)
+def test_wrong_email_or_password_authorization(
+        chromium_page: Page, email: str, password: str):
     chromium_page.goto(
-            (
-                "https://nikita-filonov.github.io/"
-                "qa-automation-engineer-ui-course/#/auth/login"
-            )
+        (
+            "https://nikita-filonov.github.io/"
+            "qa-automation-engineer-ui-course/#/auth/login"
         )
+    )
 
     email_input = chromium_page.get_by_test_id(
-            'login-form-email-input').locator('input')
-    email_input.fill('user.name@gmail.com')
+        'login-form-email-input').locator('input')
+    email_input.fill(email)
 
     password_input = chromium_page.get_by_test_id(
-            'login-form-password-input').locator('input')
-    password_input.fill('password')
+        'login-form-password-input').locator('input')
+    password_input.fill(password)
 
     login_button = chromium_page.get_by_test_id('login-page-login-button')
     login_button.click()
 
     wrong_alert = chromium_page.get_by_test_id(
-            'login-page-wrong-email-or-password-alert')
+        'login-page-wrong-email-or-password-alert')
     expect(wrong_alert).to_be_visible()
     expect(wrong_alert).to_have_text(
-            'Wrong email or password'
-        )
+        'Wrong email or password'
+    )
