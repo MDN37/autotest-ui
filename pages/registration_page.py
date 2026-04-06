bbc02d59b1b1bee5427b5566d0de2fc1a363e07c
MDN37 <45273378+MDN37@users.noreply.github.com>
2026-04-06 17:45:33 +0300

    Практикуемся в реализации PageComponent.  Практикуемся в написании компонентов с использованием паттерна PageComponent

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,3 +1,5 @@
+from components.authentication.registration_form_component import (
+    RegistrationFormComponent)
 from pages.base_page import BasePage
 from playwright.sync_api import Page
 
@@ -5,19 +7,10 @@ from playwright.sync_api import Page
 class RegistrationPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
-        self.email_input = page.get_by_test_id(
-            'registration-form-email-input').locator('input')
-        self.username_input = page.get_by_test_id(
-            'registration-form-username-input').locator('input')
-        self.password_input = page.get_by_test_id(
-            'registration-form-password-input').locator('input')
+
+        self.registration_form = RegistrationFormComponent(page)
         self.registration_button = page.get_by_test_id(
             'registration-page-registration-button')
-
-    def fill_registration_form_input(self):
-        self.email_input.fill('user@gmail.com')
-        self.username_input.fill('username')
-        self.password_input.fill('password')
 
     def click_registration_button(self):
         self.registration_button.click()
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -11,6 +11,7 @@ def test_successful_registration(
     registration_page.visit(
         "https://nikita-filonov.github.io/"
         "qa-automation-engineer-ui-course/#/auth/registration")
-    registration_page.fill_registration_form_input()
+    registration_page.registration_form.fill(
+        'user@gmail.com', 'username', 'password')
     registration_page.click_registration_button()
-    dashboard_page.check_visible_dashboard_title()
+    dashboard_page.dashboard_toolbar_view.check_visible()
